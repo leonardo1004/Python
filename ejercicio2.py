dd5f60e2f439bcc13894ae0f696df33907cd235b
LeonardoQui <leoqp123r@gmail.com>
2020-09-06 00:08:29 -0500

    Se añadio un bucle

--- ejercicio2.py
+++ ejercicio2.py
@@ -8,9 +8,10 @@ Created on Sat Sep  5 23:59:40 2020
 
 print("             ----Bienvenido----")
 print("------Calcula si el numero es par o imrpar---------")
-print("")
-numero=int(input("Ingrese un numero: "))
-if numero%2==0:
-   print("El numero es par")
-else: 
-   print("El numero es impar")
+while 1:
+    print("")
+    numero=int(input("Ingrese un numero: "))
+    if numero%2==0:
+        print("El numero es par")
+    else: 
+        print("El numero es impar")
