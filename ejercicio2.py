9c8570f9c59c917cf21736da0acdc36d9533e832
LeonardoQui <leoqp123r@gmail.com>
2020-09-06 00:06:17 -0500

    Se creo la variable numero y el condicional par o impar funcionando

--- ejercicio2.py
+++ ejercicio2.py
@@ -6,3 +6,11 @@ Created on Sat Sep  5 23:59:40 2020
 """
 
 
+print("             ----Bienvenido----")
+print("------Calcula si el numero es par o imrpar---------")
+print("")
+numero=int(input("Ingrese un numero: "))
+if numero%2==0:
+   print("El numero es par")
+else: 
+   print("El numero es impar")
