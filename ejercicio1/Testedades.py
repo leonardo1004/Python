de37176d3d1c82624bc74596a983edf941848901
LeonardoQui <leoqp123r@gmail.com>
2020-09-06 17:36:13 -0500

    Se el metodo testcalcular_edad

--- ejercicio1/Testedades.py
+++ ejercicio1/Testedades.py
@@ -2,3 +2,9 @@ import unittest
 import edades
 
 class Testedades(unittest.TestCase):
+
+	def test_calcular_edad(self):
+        	self.assertEqual(edades.calcular_edad(18, 2020), 68)
+        
+if __name__ == "__main__":
+    unittest.main()
