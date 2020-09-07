4af385912094a3ffc5841b7d241d4fab41fbfca6
LeonardoQui <leoqp123r@gmail.com>
2020-09-06 19:55:19 -0500

    test listo ejercicio1

--- ejercicio1/Testedades.py
+++ ejercicio1/Testedades.py
@@ -3,7 +3,7 @@ import edades
 
 class Testedades(unittest.TestCase):
 
-	def test_calcular_edad(self):
+	def testcalcular_edad(self):
         	self.assertEqual(edades.calcular_edad(18, 2020), 68)
         
 if __name__ == "__main__":
