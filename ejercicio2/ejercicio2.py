cb9a7395369fda58b7909a7644613b8975b72245
LeonardoQui <leoqp123r@gmail.com>
2020-09-06 20:23:15 -0500

    Ajuste tabs

--- ejercicio2/ejercicio2.py
+++ ejercicio2/ejercicio2.py
@@ -6,15 +6,15 @@ Created on Sat Sep  5 23:59:40 2020
 """
 
 def calcular(numero):
-	print("             ----Bienvenido----")
-	print("------Calcula si el numero es par o imrpar---------")
-	while 1:
-    		if numero%2==0:
-			ban=True
-    		else: 
-			ban=False
-		if ban:
-			print("El numero es par")
-		else:
-			print("El numero es impar")
-		return ban
+    print("             ----Bienvenido----")
+    print("------Calcula si el numero es par o imrpar---------")
+    while 1:
+        if numero%2==0:
+            ban=True
+        else: 
+            ban=False
+        if ban:
+            print("El numero es par")
+        else:
+            print("El numero es impar")
+        return ban
