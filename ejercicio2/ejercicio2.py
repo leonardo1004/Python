19062a1c3ab14d3f5a065f21d2847ed503503d4e
LeonardoQui <leoqp123r@gmail.com>
2020-09-06 20:02:17 -0500

    Se edito el codigo

---
+++ ejercicio2/ejercicio2.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  5 23:59:40 2020
+
+@author: Leonardo Quintero P
+"""
+
+def calcular(numero):
+	print("             ----Bienvenido----")
+	print("------Calcula si el numero es par o imrpar---------")
+	while 1:
+    		if numero%2==0:
+        		print("El numero es par")
+    		else: 
+        		print("El numero es impar")
